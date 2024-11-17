dfdacee864e7a092d592ef6bedd14bdeff263664
Kenneth Mead <48263885+Xerner@users.noreply.github.com>
2024-11-17 00:24:53 -0500

    Update git-set-submodule-settings.sh

--- scripts/git-set-submodule-settings.sh
+++ scripts/git-set-submodule-settings.sh
@@ -4,6 +4,7 @@ git config --global --add status.submodulesummary true # makes git diff show sub
 git config --global --add submodule.recurse true # makes git pull also update submodules by default
 git config --global push.recurseSubmodules check # makes git push fail if submodules have unpushed changes
 git config --global push.recurseSubmodules on-demand # makes git push also push submodule(s) changes
-git config alias.supdate 'submodule update --remote --merge --init --recursive' # adds a new git command for updating all submodules recursively with merge
-git config --global alias.scommitall "submodule foreach --recursive 'git commit'"
-git config --global alias.saddall "submodule foreach --recursive 'git add'"
+git config --global alias.supdate 'submodule update --remote --merge --init --recursive' # adds a new git command for updating all submodules recursively with merge
+git config --global alias.scheckout "submodule foreach --recursive 'git checkout'"
+git config --global alias.sadd "submodule foreach --recursive 'git add'"
+git config --global alias.scommit "submodule foreach --recursive 'git commit || :'"
